87b7e9c9dc3b6ecdee04af9ec543fa3e33b4250a
Gene R <gururise@gmail.com>
2023-02-09 09:08:21 -0800

    update to instruct test 2

--- config.py
+++ config.py
@@ -12,21 +12,21 @@ quantized = {
 # UNCOMMENT TO SELECT OPTIONS
 # Not full list of options, see https://pypi.org/project/rwkvstic/ and https://huggingface.co/BlinkDL/ for more models/modes
 
-# RWKV 1B5 instruct test 1 model
+# RWKV 1B5 instruct test 2 model
 # Approximate
 # [Vram usage: 6.0GB]
 # [File size: 3.0GB]
 
 
 config = {
-    "path": "https://huggingface.co/BlinkDL/rwkv-4-pile-1b5/resolve/main/RWKV-4-Pile-1B5-Instruct-test1-20230124.pth",
+    "path": "https://huggingface.co/BlinkDL/rwkv-4-pile-1b5/resolve/main/RWKV-4-Pile-1B5-Instruct-test2-20230209.pth",
     "mode": TORCH,
     "runtimedtype": torch.float32,
     "useGPU": torch.cuda.is_available(),
     "dtype": torch.float32
 }
 
-title = "RWKV-4 (1.5b Instruct)"
+title = "RWKV-4 (1.5b Instruct Test 2)"
 
 # RWKV 1B5 instruct model quantized
 # Approximate
